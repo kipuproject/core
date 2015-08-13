c74cef84990e80614d44cbc0c41fb12c050b3566
Karen Palacios <kjmorale@caracoltv.com.co>
2015-08-12 20:23:23 -0500

    Correción de sintaxis

--- builder/Acceso.class.php
+++ builder/Acceso.class.php
@@ -1,4 +1,4 @@
-<?
+<?php
 require_once("core/manager/Configurador.class.php");
 require_once("core/builder/builderSql.class.php");
 
@@ -6,7 +6,7 @@ require_once("core/builder/builderSql.class.php");
 class Acceso {
 
 	private static $instance;
-	
+
     public static function singleton() {
         if (!isset(self::$instance)) {
             $className = __CLASS__;
@@ -16,26 +16,22 @@ class Acceso {
     }
 
     private function __construct() {
-        $this->miConfigurador=Configurador::singleton();
-		$this->generadorClausulas=BuilderSql::singleton();
-		$conexion="master";
-		$this->miRecursoDB=$this->miConfigurador->fabricaConexiones->getRecursoDB($conexion);
+      $this->miConfigurador = Configurador::singleton();
+		  $this->generadorClausulas = BuilderSql::singleton();
+		  $conexion = "master";
+		  $this->miRecursoDB = $this->miConfigurador->fabricaConexiones->getRecursoDB($conexion);
     }
 
     public function getPermissions($permission,$rol){
-
-		$cadena=$this->generadorClausulas->cadenaSql("getAccess",array('permission'=>$permission,'rol'=>$rol));
-		$result=$this->miRecursoDB->ejecutarAcceso($cadena,"busqueda");
-		$r=0;
-		while(isset($result[$r][0])){
-			$output[$result[$r][0]]=(boolean)$result[$r][1];  
-		$r++;
+		  $cadena = $this->generadorClausulas->cadenaSql("getAccess",array('permission'=>$permission,'rol'=>$rol));
+		  $result = $this->miRecursoDB->ejecutarAcceso($cadena,"busqueda");
+		  $r=0;
+  		while(isset($result[$r][0])){
+  			$output[$result[$r][0]] = (boolean)$result[$r][1];
+  		$r++;
 		}
 		return $output;
 	}
 
-	
-}
 
-//Fin de la clase db_admin
-?>
+}
